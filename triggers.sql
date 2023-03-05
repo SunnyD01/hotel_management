296db1d1334e081fc1f4bd384eb01142c1f734b3
Yu-Chen Lee <113709950+rennylee@users.noreply.github.com>
2023-03-05 18:01:03 -0500

    Delete triggers.sql

--- triggers.sql
+++
@@ -1,8 +0,0 @@
-CREATE TRIGGER unique_email_of_hotel
-BEFORE INSERT OR UPDATE ON hotel_chain
-FOR EACH ROW
-BEGIN
-	IF (SELECT COUNT(*) FROM hotel_chain WHERE email_address = NEW.email_address AND chain_id != NEW.chain_id) > 0 THEN
-		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'The email address must be unique';
-	END IF;
-END; 
