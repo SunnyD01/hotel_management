d9c15d5bd039e3e469bd1d5782bc5267ab3c54e9
Yu-Chen Lee <113709950+rennylee@users.noreply.github.com>
2023-03-17 22:30:40 +0000

    setup for react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  const title = '~Welcome to the hotel homepage~';
+
+  return (
+    <div className="App">
+      
+      <div className="content">
+        <h1>{title}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (  
+        <nav classname="navbar">
+            <h1> The hotel room </h1>
+        </nav>
+    );
+}
+ 
+export default Navbar;
