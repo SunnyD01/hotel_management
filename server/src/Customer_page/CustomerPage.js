91c9528ae42e50a9b9d623de97eb93c5dddef413
Yu-Chen Lee <113709950+rennylee@users.noreply.github.com>
2023-03-23 02:24:19 +0000

    complete the search filter

--- server/src/Customer_page/CustomerPage.js
+++ server/src/Customer_page/CustomerPage.js
@@ -1,9 +1,15 @@
 import SearchFilter from "./SearchFilter";
+import Navbar from "../Navbar";
 
 const CustomerPage = () => {
 
     return (
-        <SearchFilter/>
+        <div>
+            <h1>Search For the Hotel you want here</h1>
+            <Navbar/>
+            <SearchFilter/>
+        </div>
+        
     );
 }
  
--- server/src/Customer_page/SearchFilter.js
+++ server/src/Customer_page/SearchFilter.js
@@ -1,8 +1,49 @@
+import {useState} from "react"
+
 const SearchFilter = () => {
-    return (
-        <div>
+    
+  const [city, setCity] = useState('');
+  const [checkin, setCheckin] = useState('');
+  const [checkout, setCheckout] = useState('');
+  const [capacity, setCapacity] = useState(1);
+
+  const [stars, setStars] = useState([]);
+  const [price, setPrice] = useState([]);
+  const [numRooms, setNumRooms] = useState([]);
+  const [amenities, setAmenities] = useState([]);
+  const [chain, setChain] = useState([]);
+
+  const handleSearch = () => {
+    console.log({
+      city,
+      checkin,
+      checkout,
+      capacity,
+      stars,
+      price,
+      numRooms,
+      amenities,
+      chain,
+    });
+  };
+  
+  return (
+      
+    <div>
+      <label htmlFor="city">City:</label>
+      <input type="text" id="city" value={city} onChange={(e) => setCity(e.target.value)} />
+      
+      <label htmlFor="checkin">Check-in:</label>
+      <input type="date" id="checkin" value={checkin} onChange={(e) => setCheckin(e.target.value)} />
+
+      <label htmlFor="checkout">Check-out:</label>
+      <input type="date" id="checkout" value={checkout} onChange={(e) => setCheckout(e.target.value)} />
+      
+      <label htmlFor="capacity">Capacity minimum:</label>
+      <input type="number" id="capacity" min="1" value={capacity} onChange={(e) => setCapacity(e.target.value)} />
+      
       <label htmlFor="stars">Minimum stars:</label>
-      <select id="stars">
+      <select id="stars" value={stars} onChange={(e) => setStars(Array.from(e.target.selectedOptions, (option) => option.value))}>
         <option value="1">1 star</option>
         <option value="2">2 stars</option>
         <option value="3">3 stars</option>
@@ -11,35 +52,46 @@ const SearchFilter = () => {
       </select>
 
       <label htmlFor="price">Price range:</label>
-      <input type="range" id="price" min="50" max="1000" />
-
+      <input type="range" id="price" min="50" max="1000" onChange={(e) => setPrice(e.target.value)}/>
+      
       <label htmlFor="numRooms">Number of rooms:</label>
-      <input type="number" id="numRooms" min="1" max="10" />
+      <input type="number" id="numRooms" min="1" max="10" onChange={(e) => setNumRooms(e.target.value)}/>
 
       <label htmlFor="amenities">Amenities:</label>
       <div id="amenities">
         <label>
-          <input type="checkbox" name="pool" value="pool" />
+          <input type="checkbox" name="pool" value="pool" onChange={(e) => setAmenities(e.target.value)}/>
           Pool
         </label>
         <label>
-          <input type="checkbox" name="gym" value="gym" />
+          <input type="checkbox" name="gym" value="gym" onChange={(e) => setAmenities(e.target.value)}/>
           Gym
         </label>
         <label>
-          <input type="checkbox" name="spa" value="spa" />
+          <input type="checkbox" name="spa" value="spa" onChange={(e) => setAmenities(e.target.value)}/>
           Spa
+        </label>
+        <label>
+          <input type="checkbox" name="tv" value="tv" onChange={(e) => setAmenities(e.target.value)}/>
+          TV
+        </label>
+        <label>
+          <input type="checkbox" name="fridge" value="firdge" onChange={(e) => setAmenities(e.target.value)}/>
+          Fridge
         </label>
       </div>
 
       <label htmlFor="chain">Hotel chain:</label>
-      <select id="chain">
+      <select id="chain" onChange={(e) => setChain(e.target.value)}>
         <option value="">Any</option>
         <option value="Marriott">Marriott</option>
         <option value="Hilton">Hilton</option>
         <option value="Hyatt">Hyatt</option>
         <option value="Accor">Accor</option>
       </select>
+
+      <button onClick={handleSearch}>Search</button>
+
     </div>
     );
 }
