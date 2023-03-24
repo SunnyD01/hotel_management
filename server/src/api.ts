d4fcb47692f3f4b5365a7781f27c4a0480695731
Dennis S <dennis.f.sun@gmail.com>
2023-03-24 14:26:22 -0400

    Add another api endpoint

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import {getHotels} from "./endpoints";
+import {getHotels,
+       login_employee} 
+    from "./endpoints";
 
 const apiRouter = express.Router();
 
 apiRouter.get("/", (req, res) => res.send(req));
 apiRouter.get("/hotel", getHotels);
+apiRouter.get("/login/employee", login_employee);
 
+const yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
 
 export default apiRouter;
+
--- server/src/endpoints.ts
+++ server/src/endpoints.ts
@@ -3,10 +3,25 @@ import express from "express";
 
 export const getHotels = async (req: express.Request, res: express.Response) => {
     try {
-        console.log("??s")
-        const hotels = await client.query("SELECT * FROM hotel");
+        const hotels = await client.query("SELECT * FROM public.hotel");
         res.json(hotels.rows);
     } catch (err:any) {
         console.error(err.message);
     }
 };
+
+export const login_employee = async (
+    req: express.Request,
+    res: express.Response,
+) => {
+    try {
+        const emp_user= req.body.user;
+        const emp_password = req.body.password;
+        const sign_in = await client.query(
+            `SELECT * FROM public.employee WHERE fname='${emp_user}' and lname='${emp_password}'`
+        );
+        return res.status(200).json(sign_in.rows);
+    } catch (err: any) {
+        console.error(err.message);
+    }
+}
