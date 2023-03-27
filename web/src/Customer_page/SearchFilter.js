b8d083b9f034ec5cfac0395c3462dcd773647380
Yu-Chen Lee <113709950+rennylee@users.noreply.github.com>
2023-03-27 05:07:18 +0000

    map for display and fix the search filter

--- web/src/Customer_page/SearchFilter.js
+++ web/src/Customer_page/SearchFilter.js
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import axios from "axios"
 
 const SearchFilter = () => {
@@ -10,24 +10,66 @@ const SearchFilter = () => {
 
   const [stars, setStars] = useState([]);
   const [price, setPrice] = useState([]);
-  const [numRooms, setNumRooms] = useState([]);
   const [amenities, setAmenities] = useState([]);
   const [chain, setChain] = useState('');
+  const [hotel, setHotel] = useState([]);
 
   const [results, setResults] = useState([]);
 
+  /*useEffect(() => {
+    const fetchData = async () => {
+       const dataplace = await fetch('//the address that we want to get data from')
+       const dataResult = await dataplace.json();
+
+       setResults(dataResult)
+    }
+
+    fetchData()
+  }, [])*/
+  const handleFilterCityChange = (event) => {
+    setCity(event.target.value);
+  }
+
+  const handleFilterCheckinChange = (event) => {
+    setCheckin(event.target.value);
+  }
+  
+  const handleFilterCheckoutChange = (event) => {
+    setCheckout(event.target.value);
+  }
+  
+  const handleFilterCapacityChange = (event) => {
+    setCapacity(event.target.value);
+  }
+  
+  const handleFilterPriceChange = (event) => {
+    setPrice(event.target.value);
+  }
+  
+  const handleFilterAmenityChange = (event) => {
+    setAmenities(event.target.value);
+  }
+  
+  const handleFilterChainChange = (event) => {
+    setChain(event.target.value);
+  }
+  
+  const handleFilterHotelChange = (event) => {
+    setHotel(event.target.value);
+  }
+  
+
   const handleSearch = async () => {
     try {
-      const response = await axios.get("/api/search", {
+      const response = await axios.get("/api's place", {
         city,
         checkin,
         checkout,
         capacity,
-        stars,
         price,
-        numRooms,
         amenities,
         chain,
+        hotel,
       });
       setResults(response.data)
     } catch (error) {
@@ -35,31 +77,34 @@ const SearchFilter = () => {
     } 
     
   };
-
-  /*const hotelList = hotel.map((hotel) => {
+  
+  const roomList = results.filter((results) => {
+    return Object.keys(handleSearch).every(key => {
+      return results[key] === handleSearch[key];
+    })
+  }).map((results) => {
     return (
-      <div key={hotel.hotel_id}>
-        <h2>{hotel.hotel_id}</h2>
-        <p>Address: {hotel.address}</p>
-        <p>Capacity: {hotel.capacity}</p>
+      <div key={results.room_id}>
+        <h2>{results.room_id}</h2>
+        <p>Capacity: {results.capacity}</p>
       </div>
     );
-  });*/
+  });
   
   return (
       
     <div>
       <label htmlFor="city">City:</label>
-      <input type="text" id="city" value={city} onChange={(e) => setCity(e.target.value)} required/>
+      <input type="text" id="city" value={city} onClick={handleFilterCityChange} required/>
       
       <label htmlFor="checkin">Check-in:</label>
-      <input type="date" id="checkin" value={checkin} onChange={(e) => setCheckin(e.target.value)} required/>
+      <input type="date" id="checkin" value={checkin} onChange={handleFilterCheckinChange} required/>
 
       <label htmlFor="checkout">Check-out:</label>
-      <input type="date" id="checkout" value={checkout} onChange={(e) => setCheckout(e.target.value)} required/>
+      <input type="date" id="checkout" value={checkout} onChange={handleFilterCheckoutChange} required/>
       
       <label htmlFor="capacity">Capacity minimum:</label>
-      <input type="number" id="capacity" min="1" value={capacity} onChange={(e) => setCapacity(e.target.value)} required/>
+      <input type="number" id="capacity" min="1" value={capacity} onChange={handleFilterCapacityChange} required/>
       
       <br/>
 
@@ -73,47 +118,43 @@ const SearchFilter = () => {
       </select>
 
       <label htmlFor="price">Price range:</label>
-      <input type="range" id="price" min="50" max="1000" onChange={(e) => setPrice(e.target.value)}/>
-      
-      <label htmlFor="numRooms">Number of rooms:</label>
-      <input type="number" id="numRooms" min="1" max="10" onChange={(e) => setNumRooms(e.target.value)}/>
+      <input type="range" id="price" min="50" max="1000" onChange={handleFilterPriceChange}/> 
 
       <br/>
 
       <label htmlFor="amenities">Amenities:</label>
       <div id="amenities">
         <label>
-          <input type="checkbox" name="pool" value="pool" onChange={(e) => setAmenities(e.target.value)}/>
+          <input type="checkbox" name="pool" value="pool" onChange={handleFilterAmenityChange}/>
           Pool
         </label>
         <label>
-          <input type="checkbox" name="gym" value="gym" onChange={(e) => setAmenities(e.target.value)}/>
+          <input type="checkbox" name="gym" value="gym" onChange={handleFilterAmenityChange}/>
           Gym
         </label>
         <label>
-          <input type="checkbox" name="spa" value="spa" onChange={(e) => setAmenities(e.target.value)}/>
+          <input type="checkbox" name="spa" value="spa" onChange={handleFilterAmenityChange}/>
           Spa
         </label>
         <label>
-          <input type="checkbox" name="tv" value="tv" onChange={(e) => setAmenities(e.target.value)}/>
+          <input type="checkbox" name="tv" value="tv" onChange={handleFilterAmenityChange}/>
           TV
         </label>
         <label>
-          <input type="checkbox" name="fridge" value="firdge" onChange={(e) => setAmenities(e.target.value)}/>
+          <input type="checkbox" name="fridge" value="firdge" onChange={handleFilterAmenityChange}/>
           Fridge
         </label>
       </div>
 
       <label htmlFor="chain">Hotel chain:</label>
-      <select id="chain" onChange={(e) => setChain(e.target.value)}>
-        <option value="">Any</option>
-        <option value="Marriott">Marriott</option>
-        <option value="Hilton">Hilton</option>
-        <option value="Hyatt">Hyatt</option>
-        <option value="Accor">Accor</option>
-      </select>
+      <input type="text" id="chain" value="chain" onChange={handleFilterChainChange}/>
+
+      <label htmlFor="hotel">Name of Hotel:</label>
+      <input type="text" id="hotel" value="hotel" onChange={handleFilterHotelChange}/>
       
       <button onClick={handleSearch}>Search</button>
+
+      {roomList}
 
     </div>
     );
