1f1a28e0691ee0ff13fe2371b4ad8a3c6efb9e72
Yu-Chen Lee <ylee153@uottawa.ca>
2023-03-29 02:02:18 -0400

    add api

--- web/src/Customer_page/BookingHistory.js
+++ web/src/Customer_page/BookingHistory.js
@@ -1,11 +1,7 @@
 import Navbar from "../Navbar";
 import { useEffect, useState } from 'react';
 import axios from "axios";
-
 const pagetitle = "Booking & Renting History"
-
-
-
 const BookingHistory = () => {
     
     const [bookings, setBookings] = useState([
@@ -26,14 +22,36 @@ const BookingHistory = () => {
         },
       ]);
 
+      const [rentings, setRentings] = useState([
+        {
+          id: 1,
+          hotelName: 'Hotel A',
+          checkInDate: '2022-06-17',
+          checkOutDate: '2022-08-05',
+          numGuests: 5,
+          customer_ssn: 235689,
+        },
+        {
+          id: 2,
+          hotelName: 'Hotel B',
+          checkInDate: '2022-05-10',
+          checkOutDate: '2022-05-15',
+          numGuests: 3,
+        },
+      ]);
+
     useEffect(() => {
-      axios.get("api")
+      axios.get('http://localhost:8000/getAllBookings')
       .then((response) => setBookings(response.data))
       .catch((error) => console.error(error));
     })
-    
-    
-    
+
+    useEffect(() => {
+      axios.get("api/renting")
+      .then((response) => setRentings(response.data))
+      .catch((error) => console.error(error));
+    })
+
     return (
         <div>
             <h1>{pagetitle}</h1>
@@ -62,6 +80,31 @@ const BookingHistory = () => {
         </table>
       ) : (
         <p>No bookings found</p>
+      )}
+       <h1>Renting History</h1>
+                {bookings.length > 0 ? (
+        <table>
+            <thead>
+            <tr>
+                <th>Hotel Name</th>
+                <th>Check-in Date</th>
+                <th>Check-out Date</th>
+                <th>Number of Guests</th>
+            </tr>
+            </thead>
+            <tbody>
+                {rentings.map((renting) => (
+                <tr key={renting.id}>
+                    <td>{renting.hotelName}</td>
+                    <td>{renting.checkInDate}</td>
+                    <td>{renting.checkOutDate}</td>
+                    <td>{renting.numGuests}</td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
+      ) : (
+        <p>No rentings found</p>
       )}
         </div>
 
