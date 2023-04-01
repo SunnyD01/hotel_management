91b39ebcb9f74d007c3ce7ab70c960a66494ea1b
Sandotra <suryadevandotra13@gmail.com>
2023-04-01 08:46:38 -0400

    Added correct sql files

--- server/sever_setup/Project_SQL_query.sql
+++ server/sever_setup/Project_SQL_query.sql
@@ -1,5 +1,5 @@
 CREATE TABLE hotel_chain(
-	chain_id SERIAL PRIMARY KEY,
+	chain_id INT PRIMARY KEY,
 	email_address VARCHAR(255) NOT NULL UNIQUE,
 	number_of_hotels INT,
 	address_co VARCHAR(255) NOT NULL UNIQUE,
@@ -7,13 +7,14 @@ CREATE TABLE hotel_chain(
 );
 
 CREATE TABLE hotel(
-	hotel_id SERIAL PRIMARY KEY,
+	hotel_id INT PRIMARY KEY,
 	manager_id INT,
 	employees INT,
 	rating INT,
 	phone_number VARCHAR(255) NOT NULL UNIQUE,
 	email_address VARCHAR(255) NOT NULL UNIQUE,
 	address VARCHAR(255) NOT NULL UNIQUE,
+	city VARCHAR(10) NOT NULL,
 	number_of_rooms INT,
 	chain_id INT NOT NULL,
 	CONSTRAINT fk_chain_id
@@ -37,17 +38,19 @@ CREATE TABLE customer(
 	ssn INT PRIMARY KEY NOT NULL UNIQUE,
 	fname VARCHAR(50) NOT NULL,
 	lname VARCHAR(50) NOT NULL,
-	address VARCHAR(255) NOT NULL UNIQUE,
-	date_of_registration DATE NOT NULL,
+	address VARCHAR(255) NOT NULL,
+	date_of_registration DATE NOT NULl,
 	booking_history TEXT
 );
 
 CREATE TABLE booking(
-	booking_id SERIAL PRIMARY KEY,
+	booking_id INT PRIMARY KEY,
 	exp_checkin DATE NOT NULL,
 	exp_checkout DATE,
-	time_of_booking DATETIME NOT NULL,
+	time_of_booking TIMESTAMP NOT NULL,
 	room_id INT NOT NULL,
+	hotel_id INT NOT NULL,
+	archive BOOLEAN NOT NULL,
 	customer_ssn INT NOT NULL,
 	CONSTRAINT fk_customer_ssn
 		FOREIGN KEY(customer_ssn)
@@ -55,19 +58,23 @@ CREATE TABLE booking(
 );
 
 CREATE TABLE renting(
-	renting_id SERIAL PRIMARY KEY,
-	checkin_date DATE,
+	renting_id INT PRIMARY KEY,
 	checkout_date DATE,
+	checkin_date DATE,
 	employee INT NOT NULL,
 	customer INT NOT NULL,
-	room_id INT NOT NULL UNIQUE,
-	CONSTRAINT fk_room_id
-		FOREIGN KEY(room_id)
-			REFERENCES room(room_id)
+	room_id INT NOT NULL,
+	booking_id INT,
+	hotel_id INT NOT NULL,
+	archive BOOLEAN NOT NULL,
+	payment VARCHAR(6) NOT NULL,
+	CONSTRAINT fk_booking_id
+		FOREIGN KEY(booking_id)
+			REFERENCES booking(booking_id)
 );
 
 CREATE TABLE room(
-	room_id SERIAL PRIMARY KEY,
+	room_id INT PRIMARY KEY,
 	renting_history TEXT,
 	type_of_view VARCHAR(50) NOT NULL,
 	capacity INT NOT NULL,
